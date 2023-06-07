bcd6de884da9c2fe0ff60441b7af9c658538da5f
Stefan Lemke <stban@posteo.de>
2023-06-07 17:37:44 +0200

    fixed naming issue

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require ('./dbconnect')
-const pokeRoutes = require("./routes/Poke")
-const resultRoutes = require("./routes/Result")
-const fighterRoutes = require("./routes/Fighter")
-
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./dbconnect");
+const pokeRoutes = require("./routes/poke");
+const resultRoutes = require("./routes/result");
+const fighterRoutes = require("./routes/fighter");
 
 const PORT = process.env.PORT || 8000;
 
-connectDB()
+connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/poke", pokeRoutes);
-app.use("/results", resultRoutes)
-app.use("/fighters", fighterRoutes)
+app.use("/results", resultRoutes);
+app.use("/fighters", fighterRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to my API")
-})
+  res.send("Welcome to my API");
+});
 
 app.listen(PORT, console.log(`My port is ${PORT}`));
